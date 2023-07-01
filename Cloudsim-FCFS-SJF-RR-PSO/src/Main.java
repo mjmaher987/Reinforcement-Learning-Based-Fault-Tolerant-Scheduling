84d15210819d3accf09138ce9d34e2d4367b2809
mjmaher987 <mjmaher987@gmail.com>
2023-07-01 17:30:09 +0330

    add some comments on the project

--- Cloudsim-FCFS-SJF-RR-PSO/src/Main.java
+++ Cloudsim-FCFS-SJF-RR-PSO/src/Main.java
@@ -5,6 +5,12 @@ import SJF.SJF_Scheduler;
 
 // Mohammad Javad Maheronnaghsh
 // CloudSim and IFogSim
+// Setup Java SDK version 16
+// Last Update: July 1st
+// Associated with Sharif University of Technology
+// Professors: Dr. Mohsen Anasari, Dr.Sepideh Safari
+// Supervisors: Abolfazl Younesi, Elyas Oustad
+
 public class Main {
     public static void main(String[] args){
         FCFS_Scheduler fcfs = new FCFS_Scheduler();
