ea370c5c365985f4335fbc577ca14d52db409ed8
mjmaher987 <mjmaher987@gmail.com>
2023-07-04 16:12:44 +0330

    - add comment to the Constants

--- Cloudsim-FCFS-SJF-RR-PSO/src/Main.java
+++ Cloudsim-FCFS-SJF-RR-PSO/src/Main.java
@@ -187,5 +187,4 @@ public class Main {
         }
         return makespan;
     }
-
 }
--- Cloudsim-FCFS-SJF-RR-PSO/src/utils/Constants.java
+++ Cloudsim-FCFS-SJF-RR-PSO/src/utils/Constants.java
@@ -1,7 +1,8 @@
 package utils;
 
+//These are the hyperparameters of the project
 public class Constants {
-    public static int NO_OF_TASKS = 200; // number of Cloudlets
-    public static int NO_OF_DATA_CENTERS = 20; // number of Datacenters;
-    public static int POPULATION_SIZE = 25; // Number of Particles.
+    public static int NO_OF_TASKS = 200; // number of Cloudlets (Tasks)
+    public static int NO_OF_DATA_CENTERS = 20; // number of Datacenters (VMs)
+    public static int POPULATION_SIZE = 25; // Number of Particles
 }
