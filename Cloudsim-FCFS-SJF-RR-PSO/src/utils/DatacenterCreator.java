4bbd6a33f1526ef3e9cffcded15cdff69451f9b9
mjmaher987 <mjmaher987@gmail.com>
2023-08-18 09:24:10 +0330

    - clean code: delete some comments

--- Cloudsim-FCFS-SJF-RR-PSO/src/utils/DatacenterCreator.java
+++ Cloudsim-FCFS-SJF-RR-PSO/src/utils/DatacenterCreator.java
@@ -14,27 +14,14 @@ public class DatacenterCreator {
 
     public static Datacenter createDatacenter(String name) {
 
-        // Here are the steps needed to create a PowerDatacenter:
-        // 1. We need to create a list to store one or more Machines
         List<Host> hostList = new ArrayList<Host>();
-
-        // 2. A Machine contains one or more PEs or CPUs/Cores. Therefore, should
-        //    create a list to store these PEs before creating a Machine.
         List<Pe> peList = new ArrayList<Pe>();
 
         int mips = 1000;
-
-        // 3. Create PEs and add these into the list.
         peList.add(new Pe(0, new PeProvisionerSimple(mips)));
-        //==========HERE=====================
-   //     peList.add(new Pe(1, new PeProvisionerSimple(mips)));
-       // peList.add(new Pe(2, new PeProvisionerSimple(mips)));
-       // peList.add(new Pe(3, new PeProvisionerSimple(mips)));
-        //==================================
 
-        //4. Create Hosts with its id and list of PEs and add them to the list of machines
         int hostId = 0;
-        int ram = 2048; //host memory (MB) ((it was 2048)) 
+        int ram = 2048; //host memory (MB)
         long storage = 1000000; //host storage
         int bw = 200000;
 
@@ -47,18 +34,8 @@ public class DatacenterCreator {
                         peList,
                         new VmSchedulerTimeShared(peList)
                 )
-        ); // This is our first machine
-        
- 
-        
-        
-        //==============
+        );
 
-
-        // 5. Create a DatacenterCharacteristics object that stores the
-        //    properties of a data center: architecture, OS, list of
-        //    Machines, allocation policy: time- or space-shared, time zone
-        //    and its price (G$/Pe time unit).
         String arch = "x86";      // system architecture
         String os = "Linux";          // operating system
         String vmm = "Xen";
@@ -72,8 +49,6 @@ public class DatacenterCreator {
         DatacenterCharacteristics characteristics = new DatacenterCharacteristics(
                 arch, os, vmm, hostList, time_zone, cost, costPerMem, costPerStorage, costPerBw);
 
-
-        // 6. Finally, we need to create a PowerDatacenter object.
         Datacenter datacenter = null;
         try {
             datacenter = new Datacenter(name, characteristics, new VmAllocationPolicySimple(hostList), storageList, 0);
